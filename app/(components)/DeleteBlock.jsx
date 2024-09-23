c9b2597acf2a9bb58f7abbbee8966b24da7322df
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-09-23 14:10:26 -0500

    Corrected import statement to retrieve x icon

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 const DeleteBlock = () => {
   return (
