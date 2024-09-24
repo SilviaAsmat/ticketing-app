df7d6e69270162cca27b4d7bd865048fa9ef5c02
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-09-24 10:16:06 -0500

    Added ticket description, category, and progress labels

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -12,8 +12,21 @@ const TicketCard = () => {
           <DeleteBlock />
         </div>
       </div>
-      <ProgressDisplay />
-      <StatusDisplay />
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">
+        this is the ticket description! Please do this ticket
+      </p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">08/31/24 11:32 pm</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
